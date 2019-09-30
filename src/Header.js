3c7c6496c61b07a664fbc26e820cf645df2c7d6e
Leo Danielsson <leo.danielsson@comhem.com>
2019-09-30 20:17:15 +0200

    initial commit

---
+++ src/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Login from './Login';
+
+export default () => (
+  <header className='App-header'>
+    ComHem.se
+    <Login />
+  </header>
+);
---
+++ src/Page.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default () => (
+  <main>
+    <h1>Com Hem till Talangprogrammet</h1>
+  </main>
+);
